541509acb324b7e658c40dfa7814c0a00544d54e
Robin De Schepper <robin.deschepper93@gmail.com>
2019-11-21 22:49:08 +0100

    added include_package_data to improve wheel

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
          "Programming Language :: Python :: 3",
          "Operating System :: OS Independent",
      ],
+     include_package_data=True,
       data_files=[
           ('templates', glob.glob(os.path.join(os.path.dirname(__file__), "astrocyte", "templates", "*")))
       ],
