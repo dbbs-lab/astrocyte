cbe9d96ed9c90ac83dc445901b8d3737516d5dcb
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-19 01:49:39 +0100

    blackened

--- astrocyte/exceptions.py
+++ astrocyte/exceptions.py
@@ -1,20 +1,26 @@
 class AstroError(Exception):
     pass
 
+
 class StructureError(AstroError):
     pass
+
 
 class BuildError(AstroError):
     pass
 
+
 class UploadError(AstroError):
     pass
+
 
 class InvalidDistributionError(UploadError):
     pass
 
+
 class InvalidMetaError(UploadError):
     pass
+
 
 class GliaApiError(UploadError):
     pass
