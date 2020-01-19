ba925ddadc8307f8a0531108d9ae3a615beabb62
Robin De Schepper <robin.deschepper93@gmail.com>
2020-01-19 19:00:49 +0100

    first test

---
+++ tests/test_cli.py
@@ -0,0 +1,36 @@
+import unittest, os, sys, argparse
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+import astrocyte.cli
+
+astrocyte.cli._exit_on_fail = False
+
+# Duck punch the argument parser so it doesn't sys.exit
+def on_argparse_error(self, message):
+    raise argparse.ArgumentError(None, message)
+
+
+argparse.ArgumentParser.error = on_argparse_error
+
+
+def run_cli_command(command):
+    argv = sys.argv
+    sys.argv = command.split(" ")
+    sys.argv.insert(0, "test_cli_command")
+    result = astrocyte.cli.astrocyte_cli()
+    sys.argv = argv
+    return result
+
+
+class TestCLI(unittest.TestCase):
+    """
+        Check if packages can be discovered.
+    """
+
+    def test_basics(self):
+        self.assertRaises(argparse.ArgumentError, run_cli_command, "doesntexist")
+
+    def test_create(self):
+        run_cli_command(
+            "create package my-test --name=my_test --author=dude --email=bruv@eyo.com"
+        )
